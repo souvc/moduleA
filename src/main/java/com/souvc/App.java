36a38e87906d004c00aee865d32ff3bd00af043b
liuhongfeng <daliu_it@163.com>
2018-05-16 23:44:25 +0800

    提交子模块

---
+++ src/main/java/com/souvc/App.java
@@ -0,0 +1,18 @@
+package com.souvc;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Hello world!
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
